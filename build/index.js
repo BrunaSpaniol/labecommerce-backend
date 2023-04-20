3cb4459ce69f3eb0bc39785973cde2e26306f79e
Bruna Spaniol <bruna.spaniol@gmail.com>
2023-04-20 17:57:26 -0300

    Exercícios de Aprofundamento Express

--- build/index.js
+++ build/index.js
@@ -41,4 +41,62 @@ app.post("/purchases", (req, res) => {
     (0, database_1.createPurchase)(userId, productId, quantity, totalPrice);
     res.status(200).send("Compra cadastrada com sucesso!");
 });
+app.put("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const newEmail = req.body.email;
+    const newPassword = req.body.password;
+    const findUser = database_1.users.find((user) => {
+        return user.id === id;
+    });
+    if (findUser) {
+        (findUser.id = id),
+            (findUser.email = newEmail || findUser.email),
+            (findUser.password = newPassword || findUser.password);
+        res.status(200).send("Atualização realizada com sucesso");
+    }
+    res.status(400).send("Usuário não encontrado");
+});
+app.put("/products/:id", (req, res) => {
+    const id = req.params.id;
+    const newName = req.body.name;
+    const newPrice = req.body.price;
+    const newCategory = req.body.category;
+    const findProduct = database_1.products.find((product) => {
+        return product.id === id;
+    });
+    if (findProduct) {
+        findProduct.id = id;
+        findProduct.name = newName || findProduct.name;
+        findProduct.price = newPrice !== null && newPrice !== void 0 ? newPrice : findProduct.price;
+        findProduct.category = newCategory || findProduct.category;
+        res.status(200).send("Atualização realizada com sucesso");
+    }
+    res.status(400).send("Usuário não encontrado");
+});
+app.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const userResult = (0, database_1.deleteUser)(id);
+    userResult < 0
+        ? res.status(404).send("Item não encontrado")
+        : (database_1.users.splice(userResult, 1),
+            res.status(200).send("Item deletado com sucesso!"));
+});
+app.delete("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const productResult = (0, database_1.deleteProduct)(id);
+    productResult < 0
+        ? res.status(404).send("Item não encontrado")
+        : (database_1.users.splice(productResult, 1),
+            res.status(200).send("Item deletado com sucesso!"));
+});
+app.get("/product/:id", (req, res) => {
+    const { id } = req.params;
+    const result = (0, database_1.getProductById)(id);
+    res.status(200).send(result);
+});
+app.get("/users/:id/purchases", (req, res) => {
+    const { id } = req.params;
+    const result = (0, database_1.getAllPurchasesFromUserId)(id);
+    res.status(200).send(result);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,11 @@ import {
   createUser,
   createProduct,
   createPurchase,
+  getAllPurchasesFromUserId,
+  deleteUser,
+  deleteProduct,
 } from "./database";
-import { TProduct, TPurchase, TUser } from "./types";
+import { category, TProduct, TPurchase, TUser } from "./types";
 
 const app = express();
 
@@ -68,4 +71,77 @@ app.post("/purchases", (req: Request, res: Response) => {
   const { userId, productId, quantity, totalPrice }: TPurchase = req.body;
   createPurchase(userId, productId, quantity, totalPrice);
   res.status(200).send("Compra cadastrada com sucesso!");
+});
+
+// putEditUserById
+app.put("/users/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+  const newEmail = req.body.email as string | undefined;
+  const newPassword = req.body.password as string | undefined;
+
+  const findUser = users.find((user) => {
+    return user.id === id;
+  });
+  if (findUser) {
+    (findUser.id = id),
+      (findUser.email = newEmail || findUser.email),
+      (findUser.password = newPassword || findUser.password);
+    res.status(200).send("Atualização realizada com sucesso");
+  }
+  res.status(400).send("Usuário não encontrado");
+});
+
+// putEditProductById
+app.put("/products/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+  const newName = req.body.name as string | undefined;
+  const newPrice = req.body.price as number | undefined;
+  const newCategory = req.body.category as category | undefined;
+
+  const findProduct = products.find((product) => {
+    return product.id === id;
+  });
+  if (findProduct) {
+    findProduct.id = id;
+    findProduct.name = newName || findProduct.name;
+    findProduct.price = newPrice || findProduct.price;
+    findProduct.category = newCategory || findProduct.category;
+    res.status(200).send("Atualização realizada com sucesso");
+    return;
+  }
+  res.status(400).send("Produto não encontrado");
+});
+
+// DeleteUserById
+app.delete("/users/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const userResult = deleteUser(id);
+  userResult < 0
+    ? res.status(404).send("Item não encontrado")
+    : (users.splice(userResult, 1),
+      res.status(200).send("Item deletado com sucesso!"));
+});
+
+// DeleteProductById
+app.delete("/products/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const productResult = deleteProduct(id);
+  productResult < 0
+    ? res.status(404).send("Item não encontrado")
+    : (users.splice(productResult, 1),
+      res.status(200).send("Item deletado com sucesso!"));
+});
+
+//getProductsById
+app.get("/product/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = getProductById(id);
+  res.status(200).send(result);
+});
+
+// getUserPurchasesbyUserid
+app.get("/users/:id/purchases", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = getAllPurchasesFromUserId(id);
+  res.status(200).send(result);
 });
